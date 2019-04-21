fe56d6e14b2f44499a2282a8618e920bf7942516
Yuka-K <ykatsuki@lcmail.lcsc.edu>
2019-04-20 19:43:51 -0700

    ぉぎんあんd registration

---
+++ RecGlobe/app/src/main/java/com/example/recruiterglobe/login_option_activity.java
@@ -0,0 +1,41 @@
+package com.example.recruiterglobe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login_option_activity extends AppCompatActivity {
+
+
+    private Button mAthlete, mCoach;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_option_activity);
+
+        mAthlete = (Button) findViewById(R.id.athlete);
+        mCoach = (Button) findViewById(R.id.coach);
+
+        mAthlete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_option_activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+        mCoach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_option_activity.this, CoachLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+
+            }
+        });
+    }
+}
