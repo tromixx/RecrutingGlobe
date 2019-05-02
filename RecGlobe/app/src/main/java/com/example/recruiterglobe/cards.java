3858f9a6ede98bdeef03ed4af943c177fe97dc03
Bego <begonaandresm@gmail.com>
2019-05-02 01:58:16 -0700

    cards &

---
+++ RecGlobe/app/src/main/java/com/example/recruiterglobe/cards.java
@@ -0,0 +1,25 @@
+package com.example.recruiterglobe;
+
+public class cards {
+    private String userId;
+    private String name;
+    public cards (String userId, String name){
+        this.userId = userId;
+        this.name = name;
+    }
+
+    public String getUserId(){
+        return userId;
+    }
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
