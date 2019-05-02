fe0f70ea6f1a36583aec6112afdd7ec53322bcb7
ctmessan <46732268+ctmessan@users.noreply.github.com>
2019-05-01 22:51:48 -0700

    created coach profile
    and implemented go back and logout

--- RecGlobe/app/src/main/java/com/example/recruiterglobe/MainActivity.java
+++ RecGlobe/app/src/main/java/com/example/recruiterglobe/MainActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
 import java.util.ArrayList;
@@ -22,12 +26,39 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, login_option_activity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logout_button) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mProfile = (Button) findViewById(R.id.profile);
+        mProfile = findViewById(R.id.profile);
 
         mProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,6 +133,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+
     }
 
+
+
+
+
 }
+
