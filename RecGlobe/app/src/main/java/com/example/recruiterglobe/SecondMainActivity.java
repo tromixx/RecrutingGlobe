23a5567cc97db01b7b2f629a541964c18e66679c
ctmessan <46732268+ctmessan@users.noreply.github.com>
2019-05-02 03:44:16 -0700

    Update SecondMainActivity.java

--- RecGlobe/app/src/main/java/com/example/recruiterglobe/SecondMainActivity.java
+++ RecGlobe/app/src/main/java/com/example/recruiterglobe/SecondMainActivity.java
@@ -9,6 +9,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -32,6 +35,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SecondMainActivity extends AppCompatActivity {
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(SecondMainActivity.this, login_option_activity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logout_button) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     private cards2 cards_data[];
     private arrayAdapter2 arrayAdapter;
