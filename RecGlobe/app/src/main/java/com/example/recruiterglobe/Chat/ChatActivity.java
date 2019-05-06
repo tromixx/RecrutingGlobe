31f6c45486878609e102d6a40e2193f1b847c5d4
tromixx <veneret@gmail.com>
2019-05-06 15:31:49 -0700

    fix
    fix

--- RecGlobe/app/src/main/java/com/example/recruiterglobe/Chat/ChatActivity.java
+++ RecGlobe/app/src/main/java/com/example/recruiterglobe/Chat/ChatActivity.java
@@ -2,15 +2,15 @@
 package com.example.recruiterglobe.Chat;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.recruiterglobe.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,140 +18,144 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 
-
 public class ChatActivity extends AppCompatActivity {
-
-    Object context;
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mChatAdaper;
-    private RecyclerView.LayoutManager mMatcheLayoutManager;
-
+    private RecyclerView.Adapter mChatAdapter;
+    private RecyclerView.LayoutManager mChatLayoutManager;
 
     private EditText mSendEditText;
+
+    private Button mSendButton;
 
     private String currentUserID, matchId, chatId;
 
     DatabaseReference mDatabaseUser, mDatabaseChat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        String key;
-        //matchId = getIntent().getExtras().getString( key:"matchId");
+        matchId = getIntent().getExtras().getString("matchId");
 
-        //currentUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        currentUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("Users").child("currentUserID").child("connections").child("matches").child(matchId).child("chatId");
+        mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID).child("connections").child("matches").child(matchId).child("ChatId");
         mDatabaseChat = FirebaseDatabase.getInstance().getReference().child("Chat");
-//key setNestedScrollEnable ChatAdapter context k List
+
         getChatId();
 
-        //mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setNestedScrollingEnabled(false);
         mRecyclerView.setHasFixedSize(false);
-        //mMatcheLayoutManager = new LinearLayoutManager(context: ChatActivity.this);
-        mRecyclerView.setLayoutManager(mMatcheLayoutManager);
-        //mChatAdaper = new ChatAdapter(getDataSetChat(), context: ChatActivity.this);
-        mRecyclerView.setAdapter(mChatAdaper);
+        mChatLayoutManager = new LinearLayoutManager(ChatActivity.this);
+        mRecyclerView.setLayoutManager(mChatLayoutManager);
+        mChatAdapter = new ChatAdapter(getDataSetChat(),ChatActivity.this);
+        mRecyclerView.setAdapter(mChatAdapter);
 
         mSendEditText = findViewById(R.id.message);
-        Button mSendButton = findViewById(R.id.send);
+        mSendButton = findViewById(R.id.send);
 
         mSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 sendMessage();
             }
-
         });
-
     }
 
     private void sendMessage() {
         String sendMessageText = mSendEditText.getText().toString();
 
-        if(!sendMessageText.isEmpty()){
+        if (!sendMessageText.isEmpty()) {
             DatabaseReference newMessageDb = mDatabaseChat.push();
 
             Map newMessage = new HashMap();
-            String k;
-            //newMessage.put( k: "createdByUser", currentUserID);
-            //newMessage.put( k: "text", sendMessageText);
+            newMessage.put("createdByUser", currentUserID);
+            newMessage.put("text", sendMessageText);
 
             newMessageDb.setValue(newMessage);
         }
         mSendEditText.setText(null);
-
     }
 
-    private void getChatId(){
+    private void getChatId() {
         mDatabaseUser.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()){
-                    //chatId = dataSnapshot.getValue().toString();
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    chatId = dataSnapshot.getValue().toString();
                     mDatabaseChat = mDatabaseChat.child(chatId);
                     getChatMessages();
                 }
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
     }
 
     private void getChatMessages() {
-        mDatabaseChat.addChildEventListener(new ChildEventListener()
-
-        {
+        mDatabaseChat.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                if (dataSnapshot.exists()){
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot.exists()) {
                     String message = null;
                     String createdByUser = null;
 
-                    dataSnapshot.child("text").getValue();//message = dataSnapshot.child("text").getValue().toString();
-                    dataSnapshot.child("createByUser").getValue();//createdByUser = dataSnapshot.child("createdByUser").getValue().toString();
+                    if (dataSnapshot.child("text").getValue() != null) {
+                        message = dataSnapshot.child("text").getValue().toString();
+                    }
+                    if (dataSnapshot.child("createdByUser").getValue() != null) {
+                        createdByUser = dataSnapshot.child("createdByUser").getValue().toString();
+                    }
 
-                    //if(message!=null && createdByUser!=null){
-                        //Boolean currentUserBoolean = false;
-                        //if(createdByUser.equals(currentUserID)){
-                            //currentUserBoolean = true;
-                        //}
-                        //ChatObject newMessage = new ChatObject(message, currentUserBoolean);
-                        //resultsChat.add(newMessage);
-                        //mChatAdaper.notifyDataSetChanged();
-
-                    //}
+                    if (message != null && createdByUser != null) {
+                        Boolean currentUserBoolean = false;
+                        if (createdByUser.equals(currentUserID)) {
+                            currentUserBoolean = true;
+                        }
+                        ChatObject newMessage = new ChatObject(message, currentUserBoolean);
+                        resultsChat.add(newMessage);
+                        mChatAdapter.notifyDataSetChanged();
+                    }
                 }
-            }
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-            }
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-            }
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
             }
         });
     }
 
 
-    //private ArrayList<ChatObject> resultsChat = new ArrayList<~>();
-    //private List<ChatObject> getDataSetChat() { return resultsChat; }
+    private ArrayList<ChatObject> resultsChat = new ArrayList<ChatObject>();
+
+    private List<ChatObject> getDataSetChat() {
+        return resultsChat;
+    }
 }
 /*
 
--- RecGlobe/app/src/main/java/com/example/recruiterglobe/Chat/ChatAdapter.java
+++ RecGlobe/app/src/main/java/com/example/recruiterglobe/Chat/ChatAdapter.java
@@ -3,54 +3,56 @@ package com.example.recruiterglobe.Chat;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.recruiterglobe.R;
 
 import java.util.List;
 
-public class ChatAdapter {
+public class ChatAdapter extends RecyclerView.Adapter<ChatViewHolders>{
     private List<ChatObject> chatList;
-    //private Context context;
+    private Context context;
 
 
     public ChatAdapter(List<ChatObject> matchesList, Context context){
         this.chatList = matchesList;
-        //this.context = context;
+        this.context = context;
     }
 
-    //@Override
-    public ChatViewHolders onCreateViewHolder(ViewGroup parent, int viewType){
+    @Override
+    public ChatViewHolders onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        ViewGroup root;
-        Object attachToRoot;
-        //View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat, root: null, attachToRoot: false);
-        //RecyclerView.LayoutParams lp = new RecyclerView, LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        //layoutView.setLayoutParams(lp);
-        //ChatViewHolders rcv = new ChatViewHolders(layoutView);
-        Object rcv = null;
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat, null, false);
+        RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutView.setLayoutParams(lp);
+        ChatViewHolders rcv = new ChatViewHolders(layoutView);
 
-
-        return (ChatViewHolders) rcv;
+        return rcv;
     }
 
 
-    //@Override
-    public void onBindViewHolder(ChatViewHolders holder, int position){
+    @Override
+    public void onBindViewHolder(ChatViewHolders holder, int position) {
         holder.mMessage.setText(chatList.get(position).getMessage());
         if(chatList.get(position).getCurrentUser()){
             holder.mMessage.setGravity(Gravity.END);
-            holder.mMessage.setTextColor(Color.parseColor( "#404040"));
-            //holder.mContainer.setBackgroundColor(Color.parseColor("#F4F4F4"));
+            holder.mMessage.setTextColor(Color.parseColor("#404040"));
+            holder.mContainer.setBackgroundColor(Color.parseColor("#F4F4F4"));
         }else{
             holder.mMessage.setGravity(Gravity.START);
-            holder.mMessage.setTextColor(Color.parseColor( "#FFFFFF"));
-            //holder.mContainer.setBackgroundColor(Color.parseColor("#2DB4C8"));
+            holder.mMessage.setTextColor(Color.parseColor("#FFFFFF"));
+            holder.mContainer.setBackgroundColor(Color.parseColor("#2DB4C8"));
         }
 
     }
 
-    //@Override
+    @Override
     public int getItemCount() {
         return this.chatList.size();
     }
 }
+
